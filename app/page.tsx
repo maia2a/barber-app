ec910eb4b98c4661ed79a0a8ed801f1cf41b57bb
Gabriell Maia <gabrielldeveloper@gmail.com>
2025-05-09 00:54:25 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-"use client"
-
-import { useState } from "react"
+import { Button } from "./_components/ui/button"
 
 const Home = () => {
-  const [] = useState()
-  return <h1 className="text-red-500">Text</h1>
+  return <Button>Test</Button>
 }
 export default Home
