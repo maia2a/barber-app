deb81d555bda77c226c1240711a73b8444babeb2
Gabriell Maia <gabrielldeveloper@gmail.com>
2025-05-12 00:36:09 -0300

    feat: add banner and input component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,36 @@
+import { SearchIcon } from "lucide-react"
+import Image from "next/image"
 import Header from "./_components/header"
+import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
 
 const Home = () => {
-  return <Header />
+  return (
+    <div>
+      {/* header */}
+      <Header />
+      {/* main content */}
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá Gabriell</h2>
+        <p className="text-sm text-gray-500">Segunda-feira, 05 de agosto</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="/banner.png"
+            alt="Agende nos melhores com FSW Barber"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 export default Home
