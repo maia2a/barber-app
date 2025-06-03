cb683132ae683305e892654212e5454bf42e04b8
Gabriell Maia <gabrielldeveloper@gmail.com>
2025-06-03 14:26:29 -0300

    refactor(barbershop-page): define BarbershopPageProps interface for improved type safety

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -13,11 +13,13 @@ import Image from "next/image"
 import Link from "next/link"
 import { notFound } from "next/navigation"
 
-export default async function BarbershopPage({
-  params,
-}: {
-  params: { id: string }
-}) {
+interface BarbershopPageProps {
+  params: {
+    id: string
+  }
+}
+
+export default async function BarbershopPage({ params }: BarbershopPageProps) {
   const { id } = params
 
   const barbershop = await db.barbershop.findUnique({
